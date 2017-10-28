b85c639f2fc619a5bb0111d556e53de7e887e392
Marko Krstic <mrkkrstic@gmail.com>
2017-10-28 09:47:25 +0200

    Initial commit

---
+++ db.go
@@ -0,0 +1,141 @@
+package memorydb
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+const (
+	identifier = "Id"
+)
+
+type Manager struct {
+	Add      func(item interface{}, collection string) string
+	FindAll  func(collection string) ([]interface{}, error)
+	FindBy   func(field, value, collection string) (interface{}, error)
+	FindById func(id string, collection string) (interface{}, error)
+	Remove   func(id, collection string) error
+	Update   func(id string, item interface{}, collection string) error
+}
+
+type col struct {
+	lastId int64
+	items  []interface{}
+}
+
+func Create() Manager {
+	db := make(map[string]col)
+	var syn sync.RWMutex
+	return Manager{
+		Add:      add(db, syn),
+		FindAll:  getAll(db),
+		FindById: findById(db),
+		FindBy:   findBy(db),
+		Remove:   remove(db, syn),
+		Update:   update(db, syn),
+	}
+}
+
+func getAll(db map[string]col) func(string) ([]interface{}, error) {
+	return func(collection string) ([]interface{}, error) {
+		c, ok := db[collection]
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("Collection %s doesn't exist", collection))
+		}
+		return c.items, nil
+	}
+}
+
+func add(db map[string]col, syn sync.RWMutex) func(interface{}, string) string {
+	return func(item interface{}, collection string) string {
+		syn.Lock()
+		defer syn.Unlock()
+		checkIdField(item)
+
+		lastId := db[collection].lastId
+		newId := lastId + 1
+		strId := strconv.FormatInt(newId, 10)
+
+		v := reflect.ValueOf(item).Elem().FieldByName(identifier)
+		v.SetString(strId)
+
+		items := append(db[collection].items, item)
+		c := col{lastId: newId, items: items}
+		db[collection] = c
+		return strId
+	}
+}
+
+func remove(db map[string]col, syn sync.RWMutex) func(string, string) error {
+	return func(id, collection string) error {
+		syn.Lock()
+		defer syn.Unlock()
+		_, index, err := find(db, identifier, id, collection)
+		if err != nil {
+			return err
+		}
+
+		items := append(db[collection].items[:index], db[collection].items[index+1:]...)
+		c := col{lastId: db[collection].lastId, items: items}
+		db[collection] = c
+
+		return nil
+	}
+}
+
+func update(db map[string]col, syn sync.RWMutex) func(string, interface{}, string) error {
+	return func(id string, item interface{}, collection string) error {
+		syn.Lock()
+		defer syn.Unlock()
+		_, index, err := find(db, identifier, id, collection)
+		if err != nil {
+			return err
+		}
+
+		v := reflect.ValueOf(item).Elem().FieldByName(identifier)
+		v.SetString(id)
+
+		db[collection].items[index] = item
+		return nil
+	}
+}
+
+func findById(db map[string]col) func(string, string) (interface{}, error) {
+	return func(id, collection string) (interface{}, error) {
+		item, _, err := find(db, identifier, id, collection)
+		return item, err
+	}
+}
+
+func findBy(db map[string]col) func(string, string, string) (interface{}, error) {
+	return func(field, value, collection string) (interface{}, error) {
+		item, _, err := find(db, field, value, collection)
+		return item, err
+	}
+}
+
+func find(db map[string]col, field, value, collection string) (interface{}, int, error) {
+	items, err := getAll(db)(collection)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	for i, v := range items {
+		if reflect.ValueOf(v).Elem().FieldByName(field).String() == value {
+			return v, i, nil
+		}
+	}
+
+	return nil, 0, errors.New(fmt.Sprintf("No Item Found With %s %s", field, value))
+}
+
+func checkIdField(item interface{}) {
+	elem := reflect.TypeOf(item).Elem()
+	val, ok := elem.FieldByName(identifier)
+	if !ok || val.Type.String() != "string" {
+		panic(elem.Name() + " must have field `" + identifier + "` type string")
+	}
+}
---
+++ db_test.go
@@ -0,0 +1,116 @@
+package memorydb
+
+import (
+	"github.com/krstak/testify"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestAdd(t *testing.T) {
+	db := Create()
+
+	user := testUser{Age: 15}
+
+	id := db.Add(&user, "users")
+
+	res, err := db.FindById(id, "users")
+	actUser := *(res.(*testUser))
+
+	testify.Nil(t)(err)
+	testify.Equal(t)(user, actUser)
+}
+
+func TestAddConcurrency(t *testing.T) {
+	db := Create()
+	var w sync.WaitGroup
+
+	v := func() {
+		user := testUser{Age: 10}
+		db.Add(&user, "users")
+		time.Sleep(1 * time.Millisecond)
+		w.Done()
+	}
+
+	userNum := 1000
+	w.Add(userNum)
+	for i := 0; i < userNum; i++ {
+		go v()
+	}
+	w.Wait()
+
+	res, err := db.FindAll("users")
+
+	testify.Nil(t)(err)
+	testify.Equal(t)(userNum, len(res))
+
+	// there must not be duplicated IDs
+	mp := make(map[string]int)
+	for i := 0; i < len(res); i++ {
+		mp[(res[i]).(*testUser).Id]++
+		testify.Equal(t)(1, mp[(res[i]).(*testUser).Id])
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	db := Create()
+
+	user := testUser{Age: 15, Name: "John"}
+	id := db.Add(&user, "users")
+
+	userUpdate := testUser{Age: 20, Name: "John"}
+	db.Update(id, &userUpdate, "users")
+
+	res, err := db.FindById(id, "users")
+	actUser := *(res.(*testUser))
+
+	testify.Nil(t)(err)
+	testify.Equal(t)(20, actUser.Age)
+	testify.Equal(t)("John", actUser.Name)
+}
+
+func TestRemove(t *testing.T) {
+	db := Create()
+	coll := "users"
+
+	user1 := testUser{Age: 15, Name: "John"}
+	id1 := db.Add(&user1, "users")
+
+	user2 := testUser{Age: 17, Name: "Lucy"}
+	id2 := db.Add(&user2, coll)
+
+	db.Remove(id1, coll)
+
+	res, err := db.FindAll(coll)
+	actUser := *(res[0].(*testUser))
+
+	testify.Nil(t)(err)
+	testify.Equal(t)(1, len(res))
+	testify.Equal(t)(id2, actUser.Id)
+	testify.Equal(t)("Lucy", actUser.Name)
+	testify.Equal(t)(17, actUser.Age)
+}
+
+func TestFindBy(t *testing.T) {
+	db := Create()
+	coll := "users"
+
+	user1 := testUser{Age: 15, Name: "John"}
+	db.Add(&user1, "users")
+
+	user2 := testUser{Age: 17, Name: "Lucy"}
+	db.Add(&user2, coll)
+
+	res, err := db.FindBy("Name", "John", coll)
+	actUser := *(res.(*testUser))
+
+	testify.Nil(t)(err)
+	testify.Equal(t)("John", actUser.Name)
+	testify.Equal(t)(15, actUser.Age)
+}
+
+type testUser struct {
+	Id   string
+	Age  int
+	Name string
+}
